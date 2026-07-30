712b75db75ef8d76c1ac008cbf08fc6a8a3a38d2
Kin-Chi <43387604+Kin-Chi@users.noreply.github.com>
2026-07-30 11:37:04 +0800

    Update setting.py

--- source/TUI/setting.py
+++ source/TUI/setting.py
@@ -193,7 +193,7 @@ class Setting(Screen):
             Label(),
             Container(
                 Select.from_values(
-                    ("AUTO", "PNG", "WEBP", "JPEG", "HEIC"),
+                    ("AUTO", "PNG", "JPEG", "HEIC"),
                     value=self.data["image_format"].upper(),
                     allow_blank=False,
                     id="image_format",
