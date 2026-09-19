ae69c2fae1cdaaca87cf5481ca463251e308cb1b
Kin-Chi <43387604+Kin-Chi@users.noreply.github.com>
2026-09-20 00:32:09 +0800

    Update static.py 9

--- source/module/static.py
+++ source/module/static.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 
-VERSION_MAJOR = 2
-VERSION_MINOR = 8
+VERSION_MINOR = 9
+VERSION_BETA = True
 VERSION_BETA = False
 __VERSION__ = f"{VERSION_MAJOR}.{VERSION_MINOR}.{'beta' if VERSION_BETA else 'stable'}"
 ROOT: Path = (
